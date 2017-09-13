5221ce0d678d94e5c757240b23ee4bc68d0769a3
Dakota Nichols <dakotachantz@gmail.com>
2017-09-13 13:19:57 -0500

    init commit added boilerplate

---
+++ src/components/EarthquakeList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import earthquakes from '../data/earthquakes';
+
+export default class EarthquakeList extends Component {
+    render() {
+        return (
+            
+        );
+    }
+}
