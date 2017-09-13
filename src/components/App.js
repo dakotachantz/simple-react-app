02a7090674bd72b63de42f7a096771f7c5953f64
Dakota Nichols <dakotachantz@gmail.com>
2017-09-13 14:53:17 -0500

    added axios to access data from api and setState on parent component to manage state

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import axios from 'axios';
 import EarthquakeInfo from './EarthquakeInfo';
 import EarthquakeList from './EarthquakeList';
 
 class App extends Component {
+  constructor(params) {
+    super(params);
+    this.state = {
+      quakes: []
+    }
+  }
+
+  componentWillMount() {
+    let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+    axios.get(url)
+      .then((response) => {
+        let quakes = response.data.features;
+        console.log('quakes: ', quakes);
+        this.setState({ quakes });
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +29,7 @@ class App extends Component {
           <div className="my-header">Earthquakes!</div>
         </div>
         <EarthquakeInfo />
-        <EarthquakeList />
+        <EarthquakeList quakes={this.state.quakes} />
       </div >
     );
   }
--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -1,38 +1,29 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import earthquakes from '../data/earthquakes';
 
 export default class EarthquakeList extends Component {
     render() {
-
-        let quakes = earthquakes.features.map((quakes, index) => {
-            return (
-                <div className="col-sm-6" key={index}>
-                    <div className="card" >
-                        <div className="card-block">
-                            <h4 className="card-title">{quakes.properties.place}</h4>
-                            <h6 className="card-subtitle mb-2 text-muted">Magnitude: {quakes.properties.mag}</h6>
-                            <h6 className="card-subtitle mb-2 text-muted">Time: {moment(quakes.properties.time).format('llll')}</h6>
-                            <p className="card-text">Coordinates: {quakes.geometry.coordinates}</p>
-
-                            <a href={quakes.properties.url} className="card-link">USGS Event Link</a>
-
-                        </div>
+        let quakes;
+        quakes = this.props.quakes.map((quake, index) => (
+            <div className="col-sm-6" key={index}>
+                <div className="card" >
+                    <div className="card-block">
+                        <h4 className="card-title">{quake.properties.place}</h4>
+                        <h6 className="card-subtitle mb-2 text-muted">Magnitude: {quake.properties.mag}</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">Time: {moment(quake.properties.time).format('llll')}</h6>
+                        <p className="card-text">Coordinates: {quake.geometry.coordinates}</p>
+                        <a href={quake.properties.url} className="card-link">USGS Event Link</a>
                     </div>
                 </div>
-
-            );
-        });
+            </div>
+        ));
 
         return (
             <div className="quake-list">
-
                 <div className="row">
                     {quakes}
                 </div>
-
             </div>
-
         );
     }
 }
